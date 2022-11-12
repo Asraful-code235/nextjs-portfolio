d6c9cbe8ce8021aad58ca86486f7d5b1d8d9bb9d
Asraful <asrafulislamcodes@gmail.com>
2022-11-12 17:29:50 +0600

    deploy

--- pages/Work.jsx
+++ pages/Work.jsx
@@ -1,52 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { AiFillGithub, AiOutlineArrowRight } from 'react-icons/ai';
 import Link from 'next/link';
-
-const data = [
-  {
-    id: 1,
-    image:
-      'https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=600',
-    name: 'Ecommerce',
-    descripton:
-      ' A web shopping market for selling products with crud functionalitys.I used Next.js,mongodb,tailwindcss,next-Auth ect.',
-    liveLInk: '',
-    githubLink: '',
-  },
-  {
-    id: 2,
-    image:
-      'https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=600',
-    name: 'Todo',
-    descripton:
-      ' A web shopping market for selling products with crud functionalitys.I used Next.js,mongodb,tailwindcss,next-Auth ect.',
-    liveLInk: '',
-    githubLink: '',
-  },
-  {
-    id: 3,
-    image:
-      'https://images.pexels.com/photos/196655/pexels-photo-196655.jpeg?auto=compress&cs=tinysrgb&w=600',
-    name: 'Todo',
-    descripton:
-      ' A web shopping market for selling products with crud functionalitys.I used Next.js,mongodb,tailwindcss,next-Auth ect.',
-    liveLInk: '',
-    githubLink: '',
-  },
-];
-const scaleVariants = {
-  whileInView: {
-    opacity: [0, 1],
-    transition: {
-      duration: 0.5,
-      ease: 'easeOut',
-    },
-  },
-};
+import { client, urlFor } from '../client';
 
 const Work = () => {
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('all');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  const handleworkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'all') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
+  useEffect(() => {
+    const query = "*[_type == 'works']";
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
   return (
     <motion.div
       id="work"
@@ -68,38 +52,62 @@ const Work = () => {
             website&apos;s.
           </p>
         </motion.div>
+        <div className="flex flex-wrap  my-4">
+          {['ui/ux', 'nextjs', 'reactjs', 'all'].map((item, index) => (
+            <div
+              key={index}
+              onClick={() => handleworkFilter(item)}
+              className={` m-2 rounded-sm  cursor-pointer capitalize px-3 py-2 flex items-center justify-center ${
+                activeFilter === item
+                  ? 'bg-[#323436] text-white font-semibold'
+                  : ' font-semibold border shadow'
+              }`}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
         <motion.div
-          whileInView={scaleVariants.whileInView}
+          animate={animateCard}
+          // whileInView={scaleVariants.whileInView}
           transition={{ duration: 0.5, delayChildren: 0.5 }}
           className=" w-full grid grid-cols-1  min-[936px]:grid-cols-2 auto-cols-auto gap-6   mt-6"
         >
-          {data.map((items, i) => (
+          {filterWork.map((work, i) => (
             <motion.div
-              key={items + i}
+              key={i}
               whileHover={{ scale: [1, 1.01] }}
               transition={{ duration: 0.5, ease: 'easeOut' }}
               className="bg-stone-50  min-w-[350px] max-w-full    rounded-md "
             >
-              <div className="bg-stone-200  relative h-[26rem]">
+              <motion.div
+                whileHover={{ opacity: [1, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: 'easeInOut',
+                  staggerChildren: 0.5,
+                }}
+                className="bg-stone-200  relative h-[26rem]"
+              >
                 <div className=" absolute bg-stone-300 inset-8 rounded-sm">
                   <img
-                    src={items.image}
-                    alt={'project'}
+                    src={urlFor(work.imgUrl)}
+                    alt={work.name}
                     className="w-full h-full rounded-lg"
                   />
                 </div>
-              </div>
+              </motion.div>
               <div className="prose ">
                 <h1 className="font-semibold text-xl mt-2 text-slate-600  ">
-                  {items.name}
+                  {work.name}
                 </h1>
                 <p className=" text-slate-600 text-base break-normal ">
-                  {items.descripton}
+                  {work.description}
                 </p>
                 <div className=" mt-4 flex justify-start items-center">
                   <button className="capitalize rounded-sm text-white  bg-[#323436] px-3 py-2 ">
                     <Link
-                      href={items.liveLInk}
+                      href={`${work.projectLink}`}
                       className="flex items-center justify-between"
                     >
                       <span className="mx-2">
@@ -110,7 +118,7 @@ const Work = () => {
                   </button>
                   <button className=" bg-[#323436] px-3 py-2   rounded-sm text-white m-2 capitalize ">
                     <Link
-                      href={items.githubLink}
+                      href={`${work.codeLink}`}
                       className="flex items-center justify-between"
                     >
                       {' '}
